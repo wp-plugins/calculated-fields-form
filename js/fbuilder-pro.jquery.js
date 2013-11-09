b1f68aaeb28db213ad4d9f9730c07b7bef295e85
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-09 16:23:32 +0000

    Add "selectable dates" feature in the date picker field
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/tags/1.0.1@801621 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/fbuilder-pro.jquery.js
+++ js/fbuilder-pro.jquery.js
@@ -112,6 +112,10 @@ myjQuery(function(){
 				items[id].dropdownRange = $(this).val();
 				reloadItems();
 			});
+			$(".working_dates input").click(function(){
+				items[id].working_dates[$(this).val()] = $(this).is(':checked');
+				reloadItems();
+			});
 			$("#sRequired").click(function(){
 				items[id].required = $(this).is(':checked');
 				reloadItems();
@@ -613,13 +617,16 @@ myjQuery(function(){
 			{
 			    for (var k=0;k<itemsDates.length;k++)
 				{
-				    if (itemsDates[k].showDropdown)
-					    $( "#"+itemsDates[k].name ).datepicker({changeMonth: true,changeYear: true,yearRange: itemsDates[k].dropdownRange,dateFormat: itemsDates[k].dformat.replace(/yyyy/g,"yy")});
-					else
-					    $( "#"+itemsDates[k].name ).datepicker({ dateFormat: itemsDates[k].dformat.replace(/yyyy/g,"yy")});
-                    $( "#"+itemsDates[k].name ).datepicker( "option", "minDate", itemsDates[k].minDate );
-                    $( "#"+itemsDates[k].name ).datepicker( "option", "maxDate", itemsDates[k].maxDate );
-                    $( "#"+itemsDates[k].name ).datepicker( "option", "defaultDate", itemsDates[k].defaultDate );
+				    var p = { dateFormat: itemsDates[k].dformat.replace(/yyyy/g,"yy")};
+				    if (itemsDates[k].showDropdown) p = $.extend(p,{changeMonth: true,changeYear: true,yearRange: itemsDates[k].dropdownRange});
+				    var dp = $( "#"+itemsDates[k].name );
+				    p = $.extend(p,{beforeShowDay: function (d) {if (!eval($(this).attr("working_dates"))[d.getDay()]) return [false,""]; else return [true,""];}});
+				    dp.datepicker(p);
+				    dp.attr("working_dates", $.stringifyXX(itemsDates[k].working_dates));
+				    dp.datepicker("option", $.datepicker.regional['it']);	    
+                    dp.datepicker( "option", "minDate", itemsDates[k].minDate );
+                    dp.datepicker( "option", "maxDate", itemsDates[k].maxDate );
+                    dp.datepicker( "option", "defaultDate", itemsDates[k].defaultDate );
 				}
                 //$(".depItem").each(function() {
 			        showHideDep(opt.identifier);
@@ -910,6 +917,7 @@ myjQuery(function(){
                 minDate:"",
                 maxDate:"",
                 defaultDate:"",
+                working_dates:[true,true,true,true,true,true,true],
 				formats:new Array("mm/dd/yyyy","dd/mm/yyyy"),
 				display:function(){
 					return '<div class="fields" id="field'+opt.identifier+'-'+this.index+'"><div class="arrow ui-icon ui-icon-play "></div><div title="Delete" class="remove ui-icon ui-icon-trash "></div><div title="Duplicate" class="copy ui-icon ui-icon-copy "></div><label>'+this.title+''+((this.required)?"*":"")+' ('+this.dformat+')</label><div class="dfield"><input class="field disabled '+this.size+'" type="text" value="'+htmlEncode(this.predefined)+'"/><span class="uh">'+this.userhelp+'</span></div><div class="clearer"></div></div>';
@@ -929,6 +937,7 @@ myjQuery(function(){
                     str += '<div><label>Min date [<a class="helpfbuilder" text="You can put one of the following type of values into this field:\n\nEmpty: No min Date.\n\nDate: A Fixed date with the same date format indicated in the &quot;Date Format&quot; drop-down field.\n\nNumber: A number of days from today. For example 2 represents two days from today and -1 represents yesterday.\n\nString: A smart text indicating a relative date. Relative dates must contain value (number) and period pairs; valid periods are &quot;y&quot; for years, &quot;m&quot; for months, &quot;w&quot; for weeks, and &quot;d&quot; for days. For example, &quot;+1m +7d&quot; represents one month and seven days from today.">help?</a>]</label><br /><input class="medium" name="sMinDate" id="sMinDate" value="'+this.minDate+'" /></div>';
                     str += '<div><label>Max date [<a class="helpfbuilder" text="You can put one of the following type of values into this field:\n\nEmpty: No max Date.\n\nDate: A Fixed date with the same date format indicated in the &quot;Date Format&quot; drop-down field.\n\nNumber: A number of days from today. For example 2 represents two days from today and -1 represents yesterday.\n\nString: A smart text indicating a relative date. Relative dates must contain value (number) and period pairs; valid periods are &quot;y&quot; for years, &quot;m&quot; for months, &quot;w&quot; for weeks, and &quot;d&quot; for days. For example, &quot;+1m +7d&quot; represents one month and seven days from today.">help?</a>]</label><br /><input class="medium" name="sMaxDate" id="sMaxDate" value="'+this.maxDate+'" /></div>';
                     str += '<div><input type="checkbox" name="sShowDropdown" id="sShowDropdown" '+((this.showDropdown)?"checked":"")+'/><label>Show Dropdown Year and Month</label><div id="divdropdownRange" style="display:'+((this.showDropdown)?"":"none")+'">Year Range [<a class="helpfbuilder" text="The range of years displayed in the year drop-down: either relative to today\'s year (&quot;-nn:+nn&quot;), absolute (&quot;nnnn:nnnn&quot;), or combinations of these formats (&quot;nnnn:-nn&quot;)">help?</a>]: <input type="text" name="sDropdownRange" id="sDropdownRange" value="'+htmlEncode(this.dropdownRange)+'"/></div></div>';
+                    str += '<div class="working_dates"><label>Selectable dates </label><br /><input name="sWD0" id="sWD0" value="0" type="checkbox" '+((this.working_dates[0])?"checked":"")+'/>Su<input name="sWD1" id="sWD1" value="1" type="checkbox" '+((this.working_dates[1])?"checked":"")+'/>Mo<input name="sWD2" id="sWD2" value="2" type="checkbox" '+((this.working_dates[2])?"checked":"")+'/>Tu<input name="sWD3" id="sWD3" value="3" type="checkbox" '+((this.working_dates[3])?"checked":"")+'/>We<input name="sWD4" id="sWD4" value="4" type="checkbox" '+((this.working_dates[4])?"checked":"")+'/>Th<input name="sWD5" id="sWD5" value="5" type="checkbox" '+((this.working_dates[5])?"checked":"")+'/>Fr<input name="sWD6" id="sWD6" value="6" type="checkbox" '+((this.working_dates[6])?"checked":"")+'/>Sa</div>';
                     return str;
                 }
 		});
