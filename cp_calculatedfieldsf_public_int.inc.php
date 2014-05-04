38cb9df2997a6364f2be1147344581c4410e9077
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-04 08:50:02 +0000

    The current update corrects an issue with possible invalid characters in the equations.
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/tags/1.0.1@907659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cp_calculatedfieldsf_public_int.inc.php
+++ cp_calculatedfieldsf_public_int.inc.php
@@ -3,7 +3,7 @@
 <link href="<?php echo plugins_url('css/stylepublic.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
 <link href="<?php echo plugins_url('css/cupertino/jquery-ui-1.8.20.custom.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
 <?php
-$raw_form_str = cp_calculatedfieldsf_get_option('form_structure', CP_CALCULATEDFIELDSF_DEFAULT_form_structure,$id);
+$raw_form_str = str_replace("\r","",str_replace("\n","",cp_calculatedfieldsf_cleanJSON(cp_calculatedfieldsf_get_option('form_structure', CP_CALCULATEDFIELDSF_DEFAULT_form_structure,$id))));
 $form_data = json_decode( $raw_form_str );
 if( is_null( $form_data ) ){
 	$json = new JSON;
@@ -17,9 +17,9 @@ if( !is_null( $form_data ) && isset( $form_data[ 1 ] ) && isset( $form_data[ 1 ]
 	print '<link href="'.esc_attr( esc_url( $templatelist[ $form_data[ 1 ][ 0 ]->formtemplate ][ 'file' ] ) ).'" type="text/css" rel="stylesheet" />';
 }
 
+$raw_form_str = str_replace('"','&quot;',esc_attr($raw_form_str));
 ?>
-
-<form name="cp_calculatedfieldsf_pform<?php echo $CP_CFF_global_form_count; ?>" id="cp_calculatedfieldsf_pform<?php echo $CP_CFF_global_form_count; ?>" action="" method="post"><input type="hidden" name="form_structure<?php echo $CP_CFF_global_form_count; ?>" id="form_structure<?php echo $CP_CFF_global_form_count; ?>" size="180" value="<?php echo str_replace('"','&quot;',str_replace("\r","",str_replace("\n","",esc_attr(cp_calculatedfieldsf_cleanJSON($raw_form_str))))); ?>" />
+<form name="cp_calculatedfieldsf_pform<?php echo $CP_CFF_global_form_count; ?>" id="cp_calculatedfieldsf_pform<?php echo $CP_CFF_global_form_count; ?>" action="" method="post"><input type="hidden" name="form_structure<?php echo $CP_CFF_global_form_count; ?>" id="form_structure<?php echo $CP_CFF_global_form_count; ?>" size="180" value="<?php echo $raw_form_str; ?>" />
 <div id="fbuilder">
   <div id="fbuilder<?php echo $CP_CFF_global_form_count; ?>">
       <div id="formheader<?php echo $CP_CFF_global_form_count; ?>"></div>
