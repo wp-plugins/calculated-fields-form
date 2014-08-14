ffd3bb183547c9c04b5f70d571a58e15b265f360
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-14 23:27:20 +0000

    The current update allows include the media controls on dependencies.
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/trunk@966087 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/fields-public/31_fbuilder.fmedia.js
+++ js/fields-public/31_fbuilder.fmedia.js
@@ -95,7 +95,7 @@
                 },
             show:function()
 				{
-						return '<div class="fields '+this.csslayout+'" id="field'+this.form_identifier+'-'+this.index+'"><div class="clearer">'+this[ '_show_'+this.sMediaType]()+'</div><span class="uh">'+this.data[ this.sMediaType].sFigcaption+'</span><div class="clearer"></div></div>';
+						return '<div class="fields '+this.csslayout+'" id="field'+this.form_identifier+'-'+this.index+'"><div class="clearer"><div class="field" id="'+this.name+'">'+this[ '_show_'+this.sMediaType]()+'</div></div><span class="uh">'+this.data[ this.sMediaType].sFigcaption+'</span><div class="clearer"></div></div>';
 				}
 		}
 	);
