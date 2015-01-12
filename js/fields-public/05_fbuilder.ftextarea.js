fdcbfcdab05926d8a542df61bcba63386e5530f6
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 00:44:09 +0000

    The current update modifies the fields: textarea and calculated fields.
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/tags/1.0.2@1065512 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/fields-public/05_fbuilder.ftextarea.js
+++ js/fields-public/05_fbuilder.ftextarea.js
@@ -15,6 +15,18 @@
 			show:function()
 				{
 					return '<div class="fields '+this.csslayout+'" id="field'+this.form_identifier+'-'+this.index+'"><label for="'+this.name+'">'+this.title+''+((this.required)?"<span class='r'>*</span>":"")+'</label><div class="dfield"><textarea '+((!/^\s*$/.test(this.rows)) ? 'rows='+this.rows : '' )+' id="'+this.name+'" name="'+this.name+'" minlength="'+(this.minlength)+'" maxlength="'+$.fbuilder.htmlEncode(this.maxlength)+'" class="field '+this.size+((this.required)?" required":"")+'">'+this.predefined+'</textarea><span class="uh">'+this.userhelp+'</span></div><div class="clearer"></div></div>';
-				}
+				},
+			val:function()
+				{
+					var e = $( '[id="' + this.name + '"]:not(.ignore)' ),
+						v = '';
+						
+					if( e.length )
+					{
+						v = e.val();
+					}
+					
+					return v;	
+				}	
 		}
 	);
