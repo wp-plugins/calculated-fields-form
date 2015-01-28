febf48a5d6201ae515308846fe0fc222f8250411
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 00:04:32 +0000

    The current update allows to define the date/time control as 12 or 24 hours
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/tags/1.0.6@1076991 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/fields-public/29_fbuilder.ffieldset.fc.js
@@ -0,0 +1,22 @@
+	$.fbuilder.controls[ 'ffieldset' ]=function(){};
+	$.extend(
+		$.fbuilder.controls[ 'ffieldset' ].prototype,
+		$.fbuilder.controls[ 'ffields' ].prototype,
+		{
+			title:"Untitled",
+			ftype:"ffieldset",
+			fields:[],
+			show:function()
+				{
+					return '<div class="fields '+this.csslayout+'" id="field'+this.form_identifier+'-'+this.index+'"><FIELDSET id="'+this.name+'">'+( ( !/^\s*$/.test( this.title ) ) ? '<LEGEND>'+this.title+'</LEGEND>' : '' )+'</FIELDSET><div class="clearer"></div></div>';
+				},
+			after_show: function()
+				{
+					$.fbuilder.controls[ 'fcontainer' ].prototype.after_show.call(this);
+				},
+			showHideDep:function( toShow, toHide )
+				{
+					$.fbuilder.controls[ 'fcontainer' ].prototype.showHideDep.call( this, toShow, toHide );
+				}
+		}	
+	);
