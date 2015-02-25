78e67d46b94b8ad03184907ca588438d2ac0f571
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 01:35:42 +0000

    The current update: * Modifies the styles associated to the forms.
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/tags/1.0.13@1098766 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/fields-admin/30_fbuilder.fdiv.fc.js
@@ -0,0 +1,37 @@
+	$.fbuilder.typeList.push(
+		{
+			id:"fdiv",
+			name:"Div",
+			control_category:10
+		}
+	);
+	$.fbuilder.controls[ 'fdiv' ]=function(){};
+	$.extend(
+		$.fbuilder.controls[ 'fdiv' ].prototype,
+		$.fbuilder.controls[ 'fcontainer' ].prototype,
+		{
+			title: 'div', 
+			ftype:"fdiv",
+			fields:[],
+			display:function()
+				{
+					return '<div class="fields '+this.name+'" id="field'+this.form_identifier+'-'+this.index+'"><div class="arrow ui-icon ui-icon-play "></div><div title="Delete" class="remove ui-icon ui-icon-trash "></div><div class="dfield"><div class="fcontainer"></div></div><div class="clearer"></div></div>';
+				},
+			showTitle:function(){ return ''; },
+			editItemEvents:function()
+				{
+					$.fbuilder.controls[ 'fcontainer' ].prototype.editItemEvents.call(this);
+				},
+			remove : function()
+				{
+					return $.fbuilder.controls[ 'fcontainer' ].prototype.remove.call(this);
+				},
+			duplicateItem: function( currentField, newField )	
+				{
+					return $.fbuilder.controls[ 'fcontainer' ].prototype.duplicateItem.call( this, currentField, newField );
+				},
+			after_show:function()
+				{
+					return $.fbuilder.controls[ 'fcontainer' ].prototype.after_show.call(this);
+				}
+	});
