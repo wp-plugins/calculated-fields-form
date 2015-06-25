0cef429a81a08bb27be47b4ef401971c7faae9d7
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-25 23:19:06 +0000

    The update allows the use of exponential notation in the equations.
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/trunk@1187832 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/fbuilder-pro-admin.jquery.js
+++ js/fbuilder-pro-admin.jquery.js
@@ -46,7 +46,7 @@
 		decimalSymbol = new RegExp( $.fbuilder.escape_symbol( ( typeof decimalSymbol == 'undefined' || /^\s*$/.test( decimalSymbol ) ) ? '.' : decimalSymbol ), 'g' );
 		
 		var t = value.replace( thousandSeparator, '' ).replace( decimalSymbol, '.' ).replace( /\s/g, '' ),
-			p = /[+-]?((\d+(\.\d+)?)|(\.\d+))/.exec( t );
+		    p = /[+\-]?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?/.exec( t );
 			
 		return ( p ) ? p[0]*1 : '"' + value.replace(/'/g, "\\'").replace( /\$/g, '') + '"';
 	};
