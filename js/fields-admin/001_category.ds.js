50be3158b224c730cce94633492d0a32a6512650
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-03 11:39:57 +0000

    The current update:
    * Improves the parsing of numbers to get correct values, even for numbers with invalid formats.
    
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/tags/1.0.43@1211760 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/fields-admin/001_category.ds.js
@@ -0,0 +1,4 @@
+$.fbuilder.categoryList[20]={
+		title : "Form Controls with Datasource Connection",
+		description : "<span style='color:#FF0000;'>List of controls to connect with an external data source: database, CSV file, users data, posts data, and taxonomies data. Only available in the <a href='http://wordpress.dwbooster.com/forms/calculated-fields-form#download' target='_blank'>Developer Version</a> of plugin</span>"
+	};
