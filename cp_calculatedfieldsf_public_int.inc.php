d1543835aae27519ac54318e378278e235c18404
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-09 23:14:40 +0000

    The current update:
    * Improves the behavior of dependencies. Excluding fields like: section breaks and page breaks from dependencies.
    * Modifies the styles in the predefined layouts.
    * Allows to disable the forms when the website is visited by search engine spiders and crawlers, increasing the website's speed.
    * Corrects an issue parsing the text values on fields.
    * The pro and developer versions allows to store the forms in cache to increase the rendering speed. This feature will be included in the free version after be tested completely in the other versions of the plugin.
    * Moves the checking of database's structure to the plugin activation process.
    
    git-svn-id: https://plugins.svn.wordpress.org/calculated-fields-form/tags/1.0.57@1242010 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cp_calculatedfieldsf_public_int.inc.php
@@ -0,0 +1,33 @@
+<?php if ( !defined('CP_AUTH_INCLUDE') ) { echo 'Direct access not allowed.';  exit; } ?>
+<link href="<?php echo plugins_url('css/stylepublic.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
+<link href="<?php echo plugins_url('css/cupertino/jquery-ui-1.8.20.custom.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
+<?php
+$form_data = cp_calculatedfieldsf_get_option( 'form_structure', CP_CALCULATEDFIELDSF_DEFAULT_form_structure, $id );
+if( !empty( $form_data ) )	
+{
+	if( isset( $form_data[ 1 ] ) && isset( $form_data[ 1 ][ 0 ] ) && isset( $form_data[ 1 ][ 0 ]->formtemplate ) )
+	{	
+		$templatelist = cp_calculatedfieldsf_available_templates();
+		if( isset( $templatelist[ $form_data[ 1 ][ 0 ]->formtemplate ] ) )
+		{
+			print '<link href="'.esc_attr( esc_url( $templatelist[ $form_data[ 1 ][ 0 ]->formtemplate ][ 'file' ] ) ).'" type="text/css" rel="stylesheet" />';
+			if( isset( $templatelist[ $form_data[ 1 ][ 0 ]->formtemplate ][ 'js' ] ) )
+			{
+				print '<script src="'.esc_attr( esc_url( $templatelist[ $form_data[ 1 ][ 0 ]->formtemplate ][ 'js' ] ) ).'"></script>';    
+			}           
+		}
+	}	
+	$form_data[ 1 ][ 'formid' ]="cp_calculatedfieldsf_pform".$CP_CFF_global_form_count;
+?>
+<form name="<?php echo $form_data[ 1 ][ 'formid' ]; ?>" id="<?php echo $form_data[ 1 ][ 'formid' ]; ?>" action="" method="post" enctype="multipart/form-data"><script>form_structure<?php echo $CP_CFF_global_form_count; ?>=<?php print str_replace( array( "\n", "\r" ), " ", json_encode( $form_data ) ); ?></script>
+<div id="fbuilder">
+  <div id="fbuilder<?php echo $CP_CFF_global_form_count; ?>">
+      <div id="formheader<?php echo $CP_CFF_global_form_count; ?>"></div>
+      <div id="fieldlist<?php echo $CP_CFF_global_form_count; ?>"></div>
+  </div>
+</div> 
+<div class="clearer"></div> 
+</form>
+<?php
+}
+?>
